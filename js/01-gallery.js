539d56c33441f28d0c2873284c7f0fe6058c8b8f
unknown <Taras>
2022-12-30 18:03:13 +0100

    add basiclightbox page logic

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,45 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from './const/gallery-items.js';
+import { HTMLElementUtilities } from './utilities/html-element.js';
+import { Modal } from './components/modal.js';
 
-console.log(galleryItems);
+const galleryEl = document.querySelector('.gallery');
+const modal = new Modal();
+
+fillGallery(galleryItems);
+
+galleryEl.addEventListener('click', handleClickEvent);
+
+function handleClickEvent(ev) {
+  const { target } = ev;
+
+  ev.preventDefault();
+
+  if (HTMLElementUtilities.isIMG(target)) {
+    const source = target.dataset.source;
+    const description = target.alt;
+    modal.open({ source, description });
+  }
+}
+
+function fillGallery(items) {
+  items.forEach(item => {
+    const galleryItemEl = createGalleryEl(item);
+    galleryEl.appendChild(galleryItemEl);
+  });
+}
+
+function createGalleryEl({ preview, original, description }) {
+  const galleryItemEl = document.createElement('div');
+  galleryItemEl.classList.add('gallery__item');
+  galleryItemEl.innerHTML = `
+    <a class="gallery__link" href="large-image.jpg">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+      />
+    </a>
+  `;
+  return galleryItemEl;
+}
