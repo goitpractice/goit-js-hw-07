c4764e7cb3c655401ecf1c3f5e64ac1160e6c17b
unknown <Taras>
2023-01-01 20:09:51 +0100

    add simplelightbox gallery implementation

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,38 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from './const/gallery-items.js';
+import { HTMLElementUtilities } from './utilities/html-element.js';
 
-console.log(galleryItems);
+let lightbox;
+const galleryEl = document.querySelector('.gallery');
+
+fillGallery(galleryItems);
+// eslint-disable-next-line no-undef
+lightbox = new SimpleLightbox('.gallery a', { captionDelay: 250 });
+
+galleryEl.addEventListener('click', handleClickEvent);
+
+function handleClickEvent(ev) {
+    const { target } = ev;
+
+    ev.preventDefault();
+
+    if (HTMLElementUtilities.isIMG(target)) {
+        lightbox.open(ev.target);
+    }
+}
+
+function fillGallery(items) {
+    items.forEach(item => {
+        const galleryItemEl = createGalleryEl(item);
+        galleryEl.appendChild(galleryItemEl);
+    });
+}
+
+function createGalleryEl({ preview, original, description }) {
+    const galleryItemEl = document.createElement('a');
+    galleryItemEl.classList.add('gallery__item');
+    galleryItemEl.href = original;
+    galleryItemEl.innerHTML = `
+        <img class="gallery__image" src="${preview}" alt="${description}" title="${description}"/>
+  `;
+    return galleryItemEl;
+}
